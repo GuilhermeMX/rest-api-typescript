315f8b01d71fc462ff096cdf0144d21f7a40555e
Guilherme M. Xavier <guilhermenen15@gmail.com>
2023-07-18 17:36:56 -0300

    create post route

--- db/migrations/20230711203732_create-transactions.ts
+++ db/migrations/20230711203732_create-transactions.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('transactions', (table) => {
     table.uuid('id').primary()
     table.text('title').notNullable()
-    table.decimal('amount,', 10, 2).notNullable()
+    table.decimal('amount', 10, 2).notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable() // Utilizando um padrão de timestamp que funcionará em qualquer banco de dados SQL e NoSQL
   })
 }
